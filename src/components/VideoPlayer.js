63d5800dd36422a7fa14581b01f0586843f96247
Daniel Vadil <dvadz@yahoo.com>
2022-03-31 15:52:04 -0400

    Added an iframe with the url of the selected video, plus the title and desciption

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 const VideoPlayer = ({ video }) => {
-  return <div>VideoPlayer</div>;
+  if (!video) return <div></div>;
+
+  return (
+    <div>
+      <div className='ui embed'>
+        <iframe
+          src={`https://www.youtube.com/embed/${video.id.videoId}`}
+          title={video.snippet.title}
+          allowFullScreen></iframe>
+      </div>
+      <div className='ui segment'>
+        <h4 className='header'>{video.snippet.title}</h4>
+        <p className='description'>{video.snippet.description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default VideoPlayer;
