572acb3069649b9a25340b9e1d28eae7ab21a081
Daniel Vadil <dvadz@yahoo.com>
2022-03-31 11:03:33 -0400

    Used state to store term with every input change

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SearchBar = ({ onFormSubmit }) => {
+  const [term, setTerm] = useState("");
+
+  const onInputChange = (e) => {
+    setTerm(e.target.value);
+  };
   return (
     <div className='ui segment'>
       <form className='ui form' onSubmit={onFormSubmit}>
         <div>
-          <input type='text' placeholder='Search Youtube Videos'></input>
+          <input
+            type='text'
+            value={term}
+            placeholder='Search Youtube Videos'
+            onChange={onInputChange}></input>
         </div>
       </form>
     </div>
