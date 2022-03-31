821f5699c5862831e9b94c442a12c37e1099b3f9
Daniel Vadil <dvadz@yahoo.com>
2022-03-31 14:16:34 -0400

    Show title and image in VideoDetail

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const VideoDetail = ({ video }) => {
-  return <div>VideoDetail</div>;
+  return (
+    <div className='item'>
+      <div className='content'>
+        <img className='ui image' src={video.snippet.thumbnails.default.url} />
+        <div className='header'>{video.snippet.title}</div>
+      </div>
+    </div>
+  );
 };
 
 export default VideoDetail;
