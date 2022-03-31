fd9fb7dff4578b38a201431366126e5115a00095
Daniel Vadil <dvadz@yahoo.com>
2022-03-31 15:00:50 -0400

    Created onClick for VideoDetail

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,8 +1,13 @@
 import React from "react";
 
 const VideoDetail = ({ video }) => {
+  const onVideoDetailClick = (event) => {
+    event.preventDefault();
+    console.log("Click", video);
+  };
+
   return (
-    <div className='item'>
+    <div className='item' onClick={onVideoDetailClick}>
       <div className='content'>
         <img className='ui image' src={video.snippet.thumbnails.default.url} />
         <div className='header'>{video.snippet.title}</div>
