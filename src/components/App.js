dbfe82bf2e60bd51f41286d462eabf516973612a
Daniel Vadil <dvadz@yahoo.com>
2022-03-31 13:32:47 -0400

    Created a VideoList component that will show the video results, had the App pass the video results to it

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../youtube";
+import VideoList from "./VideoList";
 
 const App = () => {
   const [videos, setVideos] = useState("");
@@ -8,13 +9,14 @@ const App = () => {
 
   const onFormSubmit = async (term) => {
     const results = await youtube.get("/search", { params: { q: term } });
-    setVideos = results.data.items;
+    setVideos(results.data.items);
     setSelectedVideo(results.data.items[0]);
   };
 
   return (
     <div className='ui container'>
       <SearchBar onFormSubmit={onFormSubmit} />
+      <VideoList videos={videos} />
     </div>
   );
 };
---
+++ src/components/VideoList.js
@@ -0,0 +1,7 @@
+import React, { useEffect } from "react";
+
+const VideoList = ({ videos }) => {
+  return <div>VideoList</div>;
+};
+
+export default VideoList;
