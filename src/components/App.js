385905ca1d719440ca4bef43e4b8d8eddefdbb95
Daniel Vadil <dvadz@yahoo.com>
2022-03-31 12:17:30 -0400

    Store the videos to state and also set first video as selectedVideo

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,13 @@ import SearchBar from "./SearchBar";
 import youtube from "../youtube";
 
 const App = () => {
+  const [videos, setVideos] = useState("");
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
   const onFormSubmit = async (term) => {
     const results = await youtube.get("/search", { params: { q: term } });
+    setVideos = results.data.items;
+    setSelectedVideo(results.data.items[0]);
   };
 
   return (
